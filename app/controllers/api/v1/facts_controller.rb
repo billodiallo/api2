8759d2aa5b5141e0539e9320b13b4f31a931de03
Billo Diallo <billodiallonet@gmail.com>
2021-11-04 17:17:43 +0300

    Add update, destoy methods

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::FactsController < ApplicationController
         render json: @fact
 
     end
-
+# POST METHOD POST/users
     def create 
         @fact = Fact.new(fact_params)
         if @fact.save
@@ -21,6 +21,32 @@ class Api::V1::FactsController < ApplicationController
         end
     end
 
-    
+# PUT /users/id
+
+    def update
+        @user = User.find(params[:id])
+        if @user
+            @user.update(user_params)
+            render json: {message: 'User successfully updated.'}, status: 200
+        else
+            render json: {error: 'Unable to update User'}, status: 400
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy(user_params)
+            render json: {message: 'User successfully deleted.'}, status: 200
+        else
+            render json: {error: 'Unable to delete User'}, status: 400
+        end
+    end
+
+    private
+
+    def user_params
+        params.required 
+    end
 
 end
